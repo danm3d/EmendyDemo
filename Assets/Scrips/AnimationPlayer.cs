c71e517ec0f866a19baa22a69e2dd5b8c0ea31e3
Daniel <daniel@24bitgames.com>
2017-08-15 10:31:23 +0200

    Added animation controller

---
+++ Assets/Scrips/AnimationPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Animator))]
+public class AnimationPlayer : MonoBehaviour
+{
+	protected Animator anim;
+
+	void Start ()
+	{
+		anim = GetComponent<Animator> ();
+	}
+
+
+	public void PlayAnimation ()
+	{
+		anim.SetTrigger ("PlayCube");
+	}
+}
