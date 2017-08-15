986a7a1d127acc44c390b8a8fef737116bf4dfe7
Daniel <daniel@24bitgames.com>
2017-08-15 11:02:43 +0200

    Added navmesh demo

---
+++ Assets/Scripts/AgentMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentMovement : MonoBehaviour
+{
+
+	[SerializeField]
+	protected NavMeshAgent agent;
+
+	public NavMeshAgent Agent {
+		get {
+			return agent;
+		}
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButton (0)) {
+			RaycastHit hit;
+			if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit)) {
+				Debug.DrawLine (Camera.main.transform.position, hit.point);
+				if (agent != null) {
+					agent.SetDestination (hit.point);
+				}
+			}
+		}
+	}
+}
