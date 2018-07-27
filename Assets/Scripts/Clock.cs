1525b591c74118f8133a69e0c5b8883b4bb61456
Dan at Work <danmendelowitz@gmail.com>
2018-07-27 09:47:04 +0200

    Commit clock script & Skull
    - Added calculations for hours mionutes and seconds degrees

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+
+	public Transform seconds, 
+		minutes, 
+		hours;
+
+	float secondDegrees, 
+		minuteDegrees, 
+		hourDegrees;
+
+	// Use this for initialization
+	void Start ()
+	{
+		secondDegrees = 360f / 60;
+		minuteDegrees = secondDegrees / 60f;
+		hourDegrees = minuteDegrees / 60;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
