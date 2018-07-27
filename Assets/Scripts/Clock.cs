037476a4e8b68b589cbca5125b81e0bb43b69912
billygrim <cebokhumalo86@gmail.com>
2018-07-27 11:12:50 +0200

    clock script that moves, the hands of the clock  (cebo)

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -7,18 +7,21 @@ public class Clock : MonoBehaviour
 {
 	public Transform seconds, minutes, hours;
 	float degreesPerHour = 30f;
+    float degreesperMinute = 6f;
+    float degreesPerSecond = 6f;
 
-	// Use this for initialization
+
 	void Start()
 	{
-		hours.rotation=Quaternion.Euler(0f, DateTime.Now.Hour*degreesPerHour, 0f);
-		hours.rotation = Quaternion.Euler(0f, DateTime.Now.Hour * degreesPerHour, 0f);
+      
+    }
 
-	}
-
-	// Update is called once per frame
+	
 	void Update()
 	{
+        hours.rotation = Quaternion.Euler(0f, DateTime.Now.Hour * degreesPerHour, 0f);
+        minutes.rotation = Quaternion.Euler(0f, DateTime.Now.Minute * degreesperMinute, 0f);
+        seconds.rotation = Quaternion.Euler(0f, DateTime.Now.Second * degreesPerSecond, 0f);
 
-	}
+    }
 }
