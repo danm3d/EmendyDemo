f0034e7c389869af64a18daa1ab3f02235ddc73a
Dan at Work <danmendelowitz@gmail.com>
2018-07-27 11:39:29 +0200

    Added code to play ticking

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -7,21 +7,27 @@ public class Clock : MonoBehaviour
 {
 	public Transform seconds, minutes, hours;
 	float degreesPerHour = 30f;
-    float degreesperMinute = 6f;
-    float degreesPerSecond = 6f;
+	float degreesperMinute = 6f;
+	float degreesPerSecond = 6f;
 
+	public AudioSource tickSource;
+	public AudioClip clip;
 
 	void Start()
 	{
-      
-    }
+		InvokeRepeating("PlayTickSound", 1f, 1f);
+	}
 
-	
+
 	void Update()
 	{
-        hours.rotation = Quaternion.Euler(0f, DateTime.Now.Hour * degreesPerHour, 0f);
-        minutes.rotation = Quaternion.Euler(0f, DateTime.Now.Minute * degreesperMinute, 0f);
-        seconds.rotation = Quaternion.Euler(0f, DateTime.Now.Second * degreesPerSecond, 0f);
+		hours.rotation = Quaternion.Euler(0f, DateTime.Now.Hour * degreesPerHour, 0f);
+		minutes.rotation = Quaternion.Euler(0f, DateTime.Now.Minute * degreesperMinute, 0f);
+		seconds.rotation = Quaternion.Euler(0f, DateTime.Now.Second * degreesPerSecond, 0f);
+	}
 
-    }
+	public void PlayTickSound()
+	{
+		tickSource.PlayOneShot(clip);
+	}
 }
