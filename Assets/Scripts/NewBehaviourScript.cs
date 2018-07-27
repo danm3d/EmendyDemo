59247ac18f45643992223a5e82300feb2ac83845
billygrim <cebokhumalo86@gmail.com>
2018-07-27 12:16:07 +0200

    fgdg

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    
+    
+        public Transform seconds, minutes, hours;
+
+        public bool continuous;
+
+        float degreesPerHour = 30f;
+        float degreesperMinute = 6f;
+        float degreesPerSecond = 6f;
+
+    
+
+    private void Update()
+    {
+        if (continuous)
+        {
+            UpdateContinuous();
+        }
+        else
+        {
+            UpdateDiscrete();
+        }
+    }
+
+   
+
+
+    void UpdateContinuous()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+
+        hours.rotation = Quaternion.Euler(0f, 0f, (float)time.TotalHours * degreesPerHour);
+        minutes.rotation = Quaternion.Euler(0f, 0f, (float)time.TotalMinutes * degreesperMinute);
+        seconds.rotation = Quaternion.Euler(0f, 0f, (float)time.TotalSeconds * degreesPerSecond);
+
+    }
+
+   
+
+    void UpdateDiscrete()
+    {
+        hours.rotation = Quaternion.Euler(0f, 0f, DateTime.Now.Hour * degreesPerHour);
+        minutes.rotation = Quaternion.Euler(0f, 0f, DateTime.Now.Minute * degreesperMinute);
+        seconds.rotation = Quaternion.Euler(0f, 0f, DateTime.Now.Second * degreesPerSecond);
+        
+    }
+}
+
